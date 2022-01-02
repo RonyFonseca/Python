23a8e966620e071bae833c5a74293401fc4ef782
ronyfonseca <92323447+RonyFonseca@users.noreply.github.com>
2022-01-02 20:03:19 -0300

    update

--- Aulas - py/aula07.py
+++ Aulas - py/aula07.py
@@ -1,10 +1,10 @@
 n1 = int(input("Digite o primerio número?:"))
 n2 = int(input("Digite o segundo número?:"))
-s = n1 + n2
-m = n1 * n2
-d = n1 / n2
-di = n1 // n2
-resto_d = n1 % n2
-pot = n1 ** n2
+s = n1 + n2 #soma
+m = n1 * n2 #multiplicação
+d = n1 / n2 #divisão
+di = n1 // n2 #divisão inteira
+resto_d = n1 % n2 #resto da divisão
+pot = n1 ** n2 #potencia
 print("A multiplicação é {}, a divisão é {:.2f}".format(m,d))
 print("A divisão inteira é {} e o resto da divisão é {}, a potencia {}".format(di, resto_d, pot))
---
+++ Exercicios - py/ex014.py
@@ -0,0 +1,3 @@
+pergunta = float(input("Qual o salário atual : R$ "))
+calculo_salarial = (15/100 * pergunta ) + pergunta
+print("Com 15% de aumento ele será de {:.2f}".format(calculo_salarial))
